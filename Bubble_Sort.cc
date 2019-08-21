4608bd32dcfea1754f83bd921e7c903b86598494
vid58 <40040775+vid58@users.noreply.github.com>
2019-08-21 15:02:48 -0500

    Parte_1
    Los datos entrando al arreglo. 📦

--- Bubble_Sort.cc
+++ Bubble_Sort.cc
@@ -5,28 +5,18 @@ using namespace std;
 int main()
 {
     int a[50],n,temp;
-    cout<<"Ingrese el tamaño de matriz: ";
+    cout<<"Ingrese el tamaño del Arreglo: ";
     cin>>n;
-    cout<<"Ingrese los elementos de la matriz: ";
+    cout<<"Ingrese los elementos de la Arreglo: ";
 
-    for(int i=0;i<n;++i)
-        cin>>a[i];
-
-    for(int i=1;i<n;++i)
-    {
-        for(int j=0;j<(n-i);++j)
-            if(a[j]>a[j+1])
-            {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
+    for(int i=0;i<n;++i) {
+        cin >> a[i];
     }
 
-    cout<<"Matriz después de la clasificación de burbujas:";
-    for(int i=0;i<n;++i)
-        cout<<" "<<a[i];
-
+    cout<<"\nArreglo: ";
+    for(int i=0;i<n;++i) {
+        cout << " " << a[i];
+    }
     return 0;
 }
 
