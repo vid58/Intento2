e352b9941459a978cff721deb068216f85b44346
vid58 <40040775+vid58@users.noreply.github.com>
2019-08-21 15:06:33 -0500

    Completo_
    Código de ordenamiento.

--- Bubble_Sort.cc
+++ Bubble_Sort.cc
@@ -5,28 +5,30 @@ using namespace std;
 int main()
 {
     int a[50],n,temp;
-    cout<<"Ingrese el tamaño de matriz: ";
+    cout<<"Ingrese el tamaño del arreglo: ";
     cin>>n;
-    cout<<"Ingrese los elementos de la matriz: ";
+    cout<<"Ingrese los elementos del arreglo: ";
 
-    for(int i=0;i<n;++i)
-        cin>>a[i];
+    for(int i=0;i<n;++i) {
+        cin >> a[i];
+    }
 
     for(int i=1;i<n;++i)
     {
-        for(int j=0;j<(n-i);++j)
-            if(a[j]>a[j+1])
-            {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
+        for(int j=0;j<(n-i);++j) {
+            if (a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
             }
+        }
     }
 
-    cout<<"Matriz después de la clasificación de burbujas:";
-    for(int i=0;i<n;++i)
-        cout<<" "<<a[i];
-
+    //Bubble_Sort
+    cout<<"\nArreglo después de la clasificación de burbujas: ";
+    for(int i=0;i<n;++i) {
+        cout << " " << a[i];
+    }
     return 0;
 }
 
