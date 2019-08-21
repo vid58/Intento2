e273198ada216a97a2c41408b1fcae4f05b8d0bb
vid58 <40040775+vid58@users.noreply.github.com>
2019-08-21 15:29:50 -0500

    vaciado

--- Bubble_Sort.cc
+++ Bubble_Sort.cc
@@ -1,18 +1,1 @@
-#include<iostream>
-
-using namespace std;
-
-int main()
-{
-    int a[50],n,temp;
-    cout<<"Ingrese el tamaño del arreglo: ";
-    cin>>n;
-    cout<<"Ingrese los elementos del arreglo: ";
-
-    for(int i=0;i<n;++i){
-        cin>>a[i];
-    }
-    
-    return 0;
-}
 
